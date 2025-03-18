ec71df005b93c005a85eef4d420f8d7b31f83585
Carlos <carloscuba014@gmail.com>
2025-03-18 15:52:44 -0400

    move modal import above utils

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { statusColors } from '../../utils/constants';
 import Modal from '../Modal';
+import { statusColors } from '../../utils/constants';
 import { capitalize } from '../../utils/capitalize';
 
 interface ItemType {
