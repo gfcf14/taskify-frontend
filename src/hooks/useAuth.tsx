9069070ddb281eace902cf8d38f5946875431c1e
Carlos <carloscuba014@gmail.com>
2025-03-20 18:03:35 -0400

    make useauth hook check token expiry

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,9 +1,31 @@
+import { jwtDecode } from 'jwt-decode';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+const isExpired = (token: string | null) => {
+  if (!token) return true;
+
+  try {
+    const decoded: { exp: number } = jwtDecode(token);
+    return decoded.exp * 1000 < Date.now();
+  } catch (error) {
+    console.error(error);
+    return true;
+  }
+}
 
 export const useAuth = () => {
   const navigate = useNavigate();
 
   const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (isExpired(token)) {
+      localStorage.removeItem('token');
+      navigate('/');
+    }
+  }, [navigate, token]);
+
   return {
     navigate,
     token
